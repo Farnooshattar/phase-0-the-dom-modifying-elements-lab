eb91562849bf0c3e02332241f2ec7489c0dda2aa
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-04-06 12:02:18 -0400

    complete all

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+// for (let i=0;i<3;i++)
+// {
+//     const li=document.createElement("li")
+//     li.textContent=(i+1).toString()
+//     ul.append(li)
+// }
+// element.append(ul)
+const elementMain = document.getElementById("main");
+// elementMain.style.height = "300px";
+// elementMain.style.backgroundColor = "#27647B";
+// elementMain.className="fClass"
+// elementMain.classList.add("cat", "sale");
+// elementMain.classList.remove("cat")
+elementMain.remove()
+const newHeader = document.createElement("h1");
+newHeader.id='victory'
+newHeader.textContent='Farnoosh Tavakkoli is the champion'
